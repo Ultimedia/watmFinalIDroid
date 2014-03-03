e309f9ac6346468a61e43048f513aeffb2df17af
Maarten Bressinck <maartenbressinck@MacBook-Pro-van-Maarten.local>
2014-03-03 16:49:45 +0100

    initial commit

---
+++ platforms/android/ant-build/WeAppToMoved-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/maartenbressinck/watmLaunch/WeAppToMove/platforms/android/ant-build/WeAppToMoved-debug-unaligned.apk : \
+/Users/maartenbressinck/watmLaunch/WeAppToMove/platforms/android/ant-build/WeAppToMoved.ap_ \
+/Users/maartenbressinck/watmLaunch/WeAppToMove/platforms/android/ant-build/classes.dex \
+/Users/maartenbressinck/watmLaunch/WeAppToMove/platforms/android/CordovaLib/ant-build/classes.jar \
